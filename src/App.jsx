2f2898b5316483c61a5b2ca8de52c9c40e67336f
Clément Champouillon <clement@champouillon.com>
2019-09-02 00:02:04 +0200

    feat(darkMode): adding a darkmode system 🌙 (#12)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { css, Global } from '@emotion/core';
 import { ThemeProvider } from 'emotion-theming';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon } from '@fortawesome/free-solid-svg-icons';
+import styled from '@emotion/styled';
 import Home from './pages/home';
 import { themes } from '../config';
 import Footer from './components/footer';
@@ -29,18 +32,38 @@ const globalStyles = css`
   }
 `;
 
-const App = () => (
-  <ThemeProvider theme={themes[0]}>
-    <Global styles={globalStyles} />
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="/portfolio" component={Portfolio} />
-      <Route exact path="/contact" component={Contact} />
-      <Route exact path="/legal" component={Legal} />
-    </Switch>
-    <Footer />
-  </ThemeProvider>
-);
+const DarkButton = styled('button')`
+  position: fixed;
+  right: 1rem;
+  top: 40vh;
+  background: none;
+  border: none;
+  color: white;
+  cursor: pointer;
+`;
+
+const darkModeHandler = (theme, darkMode) => (darkMode
+  ? { ...theme, colors: { ...theme.colors, white: theme.colors.black, black: theme.colors.white } }
+  : theme);
+
+const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  // const toggleDarkMode = () => setDarkMode(!darkMode);
+  return (
+    <ThemeProvider theme={darkModeHandler(themes[0], darkMode)}>
+      {/* <DarkButton onClick={toggleDarkMode}><FontAwesomeIcon icon={faMoon} /></DarkButton> */}
+      <Global styles={globalStyles} />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/legal" component={Legal} />
+      </Switch>
+      <Footer />
+    </ThemeProvider>
+  );
+};
 
 export default App;
