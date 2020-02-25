d1927a91c207fbfe937c62aa1a2fcb76c8ec0c91
Clément Champouillon <clement@champouillon.com>
2020-02-25 19:50:45 +0100

    feat(portfolio): add video support (#34)

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -7,16 +7,24 @@ import Card from '../atoms/card';
 import PortfolioDetails from './portfolioDetails';
 
 const Portfolio = () => {
-  const [entries, setEntries] = useState([]);
+  const [photos, setPhotos] = useState([]);
+  const [videos, setVideos] = useState([]);
 
   const fetchData = async () => {
     try {
+      const videos = await axios({
+        method: 'get',
+        url: 'https://cockpit.cchampou.me/api/collections/get/videos?token=573f5f5bdc3eb2fff9a26a57b4fb39',
+      });
+      if (videos.data && videos.data.entries) {
+        setVideos(videos.data.entries);
+      }
       const res = await axios({
         method: 'get',
         url: 'https://cockpit.cchampou.me/api/collections/get/creations?token=573f5f5bdc3eb2fff9a26a57b4fb39',
       });
       if (res.data && res.data.entries) {
-        setEntries(res.data.entries);
+        setPhotos(res.data.entries);
       }
     } catch (e) {
       console.error(e);
@@ -27,16 +35,46 @@ const Portfolio = () => {
     fetchData();
   }, []);
 
+  console.log(photos,videos);
   return (
     <>
       <Header noWrap color="white" bgColor="pink" />
-      <Route exact path="/portfolio/:id" render={(props) => <PortfolioDetails entries={entries} {...props} />} />
+      <Route exact path="/portfolio/:id" render={(props) => <PortfolioDetails entries={[...photos, ...videos]} {...props} />} />
+      <Row padded>
+        <h1>Videos</h1>
+      </Row>
+      <Row padded>
+        <Item>
+          <Column>
+            {videos.filter((entry, i) => i % 3 === 0).map(({ thumbnail: { path }, _id: id }) => (
+                  <Card img={`https://cockpit.cchampou.me/storage/uploads${path}`} id={id} />
+              ))}
+          </Column>
+        </Item>
+        <Item>
+          <Column>
+            {videos.filter((entry, i) => i % 3 === 1).map(({ thumbnail: { path }, _id: id }) => (
+                <Card img={`https://cockpit.cchampou.me/storage/uploads${path}`} id={id} />
+            ))}
+          </Column>
+        </Item>
+        <Item>
+          <Column>
+            {videos.filter((entry, i) => i % 3 === 2).map(({ thumbnail: { path }, _id: id }) => (
+                <Card img={`https://cockpit.cchampou.me/storage/uploads${path}`} id={id} />
+            ))}
+          </Column>
+        </Item>
+      </Row>
+      <Row padded>
+        <h1>Photos</h1>
+      </Row>
       <Row padded wrap justify="center">
         <Item>
           <Column>
-            {entries
+            {photos
               .filter((entry, i) => i % 3 === 0).map(({ title, thumbnail: { path }, _id: id }) => (
-                <Card img={`https://cockpit.cchampou.me/${path}`} id={id}>
+                <Card img={`https://cockpit.cchampou.me${path}`} id={id}>
                   {title}
                 </Card>
               ))}
@@ -44,9 +82,9 @@ const Portfolio = () => {
         </Item>
         <Item>
           <Column>
-            {entries
+            {photos
               .filter((entry, i) => i % 3 === 1).map(({ title, thumbnail: { path }, _id: id }) => (
-                <Card img={`https://cockpit.cchampou.me/${path}`} id={id}>
+                <Card img={`https://cockpit.cchampou.me${path}`} id={id}>
                   {title}
                 </Card>
               ))}
@@ -54,9 +92,9 @@ const Portfolio = () => {
         </Item>
         <Item>
           <Column>
-            {entries
+            {photos
               .filter((entry, i) => i % 3 === 2).map(({ title, thumbnail: { path }, _id: id }) => (
-                <Card img={`https://cockpit.cchampou.me/${path}`} id={id}>
+                <Card img={`https://cockpit.cchampou.me${path}`} id={id}>
                   {title}
                 </Card>
               ))}
