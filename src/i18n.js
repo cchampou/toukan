c0f027989e36a27518ba57cc44d0730c05475d3b
cchampou <clement@champouillon.com>
2020-02-26 19:20:10 +0100

    Fix linter

--- src/i18n.js
+++ src/i18n.js
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import fr from '../lang/fr';
-import en from '../lang/en';
+import fr from '../lang/fr.json';
+import en from '../lang/en.json';
 
 i18n
   .use(initReactI18next)
