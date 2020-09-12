5082a75d993dac58953b1a03073a19dde722dd09
cchampou <clement@champouillon.com>
2020-09-12 23:14:52 +0200

    Fix logo

--- src/atoms/logo.jsx
+++ src/atoms/logo.jsx
@@ -3,15 +3,21 @@ import { Link } from 'react-router-dom';
 import styled from '@emotion/styled';
 
 import logo from '../../assets/images/logo.webp';
+import logoPng from '../../assets/images/logo.png';
 import logoLight from '../../assets/images/logo-light.png';
 
 
-const StyledLogo = styled('img')`
+const StyledLogo = styled('picture')`
   height: ${({ height }) => height || 'auto'};
   width: ${({ width }) => width || 'auto'};
 `;
 
 // eslint-disable-next-line react/jsx-props-no-spreading
-const Logo = ({ light, ...props }) => <Link to="/"><StyledLogo src={light ? logoLight : logo} alt="Logo Toukan" {...props} /></Link>;
+const Logo = ({ light, ...props }) => <Link to="/">
+  <StyledLogo>
+    <source srcSet={logo} type="image/webp" />
+    <img src={light ? logoLight : logoPng} alt="Logo Toukan" {...props} />
+  </StyledLogo>
+</Link>;
 
 export default Logo;
